a5ac978e846e17bd59abed3543a4dbe8a91c45ea
Roey Kudra <ko.roey@gmail.com>
2018-04-12 20:49:56 +0300

    Roey: Create react app project base

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Login Challenge</h1>
+        </header>
+        <p className="App-intro">
+          It is just an example
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
